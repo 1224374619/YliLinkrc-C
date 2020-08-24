76a84e76381c383fb861145bd0c2937fcd32fe13
1224374619@qq.com <1224374619@qq.com>
2020-08-24 19:29:57 +0800

    13

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     publicPath: './',
     lintOnSave: true,
     devServer: {
-        port: 443,
+        port: 8082,
         host: 'localhost',
         https: false,
         open: true,
